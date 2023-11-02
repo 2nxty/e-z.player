376533035f513cabdb320dd25bfb0a486311656a
991 <72177472+Ruan991@users.noreply.github.com>
2023-11-02 16:22:23 -0300

    Add files via upload

--- docs/js/api.js
+++ docs/js/api.js
@@ -6,11 +6,16 @@ const urlpa = new URLSearchParams(param);
 if (urlpa.has('w')) {
     const usr = urlpa.get('w');
     document.title = "E-Z - " + usr;
-    //if (words.includes(usr)) {
-        document.getElementById("ifm").src = link1 + usr;
-    //} else {
-        //window.location.replace(link);
-    //}
-} else {
-    //window.location.replace(link1);
+    document.getElementById("ifm").src = link1 + usr;
 }
+else{
+    window.location.replace(link);
+}
+
+const image_array = ['1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png']
+random_index = Math.floor(Math.random() * image_array.length);
+selected_image = image_array[random_index]
+document.getElementById("bgi").style.backgroundImage = `url('img/${selected_image}')`;
+
+
+
